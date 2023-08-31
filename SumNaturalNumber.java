2d5d4c16f3f69c62d2d38b053eeeb236757b7f9b
AniketDighe1702 <143707469+AniketDighe1702@users.noreply.github.com>
2023-08-31 17:34:31 +0530

    Add files via upload

---
+++ SumNaturalNumber.java
@@ -0,0 +1,18 @@
+package Aniket;
+
+public class SumNaturalNumber {
+
+	    public static void main(String[] args) {
+	       int n = 10; 
+	       int sum = 0;
+	        
+	       for (int i = 1; i <= n; i++) {
+	           sum += i;
+	       }
+	        
+	       System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+	   }
+}
+
+
+
